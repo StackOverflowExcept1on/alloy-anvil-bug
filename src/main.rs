ca69653f1c55ff4555fc2f9b6b1a6ca07a0dea48
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2024-07-19 14:52:26 +0300

    remove tracing and time

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,11 @@ use alloy::{
 };
 use anyhow::Result;
 use futures::StreamExt;
-use time::{
-    macros,
-    util::local_offset::{self, Soundness},
-    UtcOffset,
-};
-use tracing_subscriber::{
-    filter::{EnvFilter, LevelFilter},
-    fmt::time::OffsetTime,
-};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    unsafe { local_offset::set_soundness(Soundness::Unsound) };
-
-    tracing_subscriber::fmt()
-        .with_timer(OffsetTime::new(
-            UtcOffset::current_local_offset().unwrap_or(UtcOffset::UTC),
-            macros::format_description!(
-                "[year]-[month]-[day] [hour]:[minute]:[second].[subsecond digits:3]"
-            ),
-        ))
-        .with_env_filter(
-            EnvFilter::builder()
-                .with_default_directive(LevelFilter::INFO.into())
-                .from_env_lossy(),
-        )
-        .init();
-
-    let anvil = Anvil::new().block_time(1).try_spawn()?;
+    #[allow(unused_mut)]
+    let mut anvil = Anvil::new().block_time_f64(0.001).try_spawn()?;
     let provider = ProviderBuilder::new()
         .on_builtin(&anvil.ws_endpoint())
         .await?;
@@ -40,9 +16,23 @@ async fn main() -> Result<()> {
     let block_subscription = provider.subscribe_blocks().await?;
     let mut block_stream = block_subscription.into_stream();
 
+    //workaroud:
+    /*let child = anvil.child_mut();
+    let stdout = child.stdout.take().unwrap();
+
+    tokio::task::spawn(async {
+        use std::io::{BufRead, BufReader};
+
+        let mut reader = BufReader::new(stdout);
+        loop {
+            let mut line = String::new();
+            reader.read_line(&mut line).unwrap();
+        }
+    });*/
+
     while let Some(block) = block_stream.next().await {
         let block_number = block.header.number.expect("failed to get block number");
-        tracing::info!("block_number = {block_number}");
+        println!("block_number = {block_number}");
     }
 
     Ok(())
